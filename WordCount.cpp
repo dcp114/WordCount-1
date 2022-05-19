201d581a3b6d8942e1860452bcc2909a6e343302
ltl531124 <105534369+ltl531124@users.noreply.github.com>
2022-05-19 12:03:17 +0800

    Update WordCount.cpp

--- WordCount.cpp
+++ WordCount.cpp
@@ -1,99 +1,34 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-int CountW(char *file)//·µ»ØÎÄ¼ş´ÊµÄÊıÄ¿ 
-{  
-	FILE *fp;
-    char buffer[5000];
-    char c;
-    int bufferlen=0;
-    int LastComma = 0; // ÉÏ¸ö×Ö·ûÊÇ·ñÊÇ¿Õ¸ñ»ò¶ººÅ(1±íÊ¾ÊÇ  0±íÊ¾²»ÊÇ)
-    int WordCount=0;
-    int i;
-    if((fp=fopen(file,"r"))==NULL)
-	{
-        printf("ÎÄ¼ş²»ÄÜ´ò¿ª\n");
-        exit(0);
-    }
-	while(fgets(buffer,1000,fp)!=NULL)
-	{	
-        bufferlen=strlen(buffer);
-        for(i=0;i<bufferlen;i++)
-		{
-            c=buffer[i];
-            if(c==' ' ||c==',')
-			{
-                if(LastComma==0)
-				{
-                    WordCount++;
-                }
-                LastComma=1;
-            }
-			else if(c!='\n'&&c!='\r')
-			{
-                LastComma=0;
-            }
- 
-        } 
-        if(LastComma==0)
-            WordCount++;
-        LastComma=1;
-    }
-    fclose(fp); 
-		printf("WordNum£º%d\n",WordCount);                     	
-}
-int CountC(char *file) //·µ»ØÎÄ¼şµÄ×Ö·ûÊı 
-	{   
-		FILE *fp;
-		int CharCount=0;
-    	fp = fopen(file, "r");
-    	char a;
-    	if(NULL==(fp=fopen(file,"r")))
-    		{
-        		printf("ÎÄ¼şÎª¿Õ");
-    		}
-    	while (!feof(fp))
-    		{
-        		a=fgetc(fp);
-                	CharCount++;
-    		}
-    		
-    	fclose(fp);
-		printf("CharNum:%d ",CharCount-1);//×÷ÒµÖĞÃ»ÓĞËµÃ÷ »Ø³µ·û'\r' Ëã×Ö·û°É £¬ËùÒÔ¼õÈ¥»Ø³µ·û´øÀ´µÄ×Ö·ûÊı 
-	}
-	
-int main(int argc, char* argv[])             
+#include <stdio.h>
+#include <string.h>
+int main (int argc, char *argv[])
 {
+    // argc è¡¨ç¤ºå‚æ•°ä¸ªæ•°, *argv[]æŒ‡é’ˆæ•°ç»„, æ¯ä¸€ä¸ªæŒ‡é’ˆæŒ‡å‘ä¸€ä¸ªå­—ç¬¦ä¸²
     FILE *fp;
-    while(1)
+    int cnt = 0; // ç»Ÿè®¡æ•°
+    if ((fp=fopen(argv[2], "r")) == NULL)
     {
-    	if(strcmp(argv[1],"-h")&&strcmp(argv[1],"-w")&&strcmp(argv[1],"-c"))
-    		{
-            	printf("Ã»ÓĞ¸Ã¹¦ÄÜ\n!ÌáÊ¾!\n-w ¶Á³öµ¥´ÊÊıÁ¿\n-c ¶Á³ö×Ö·ûÊıÁ¿\n"); 
-            	return 0;
-    		}
-		if(!strcmp(argv[1],"-h"))
-    		{
-		    	printf("\n-w ¶Á³öµ¥´ÊÊıÁ¿\n-c ¶Á³ö×Ö·ûÊıÁ¿\n");
-		    	return 0;
-			}
-        if((fp=fopen(argv[2],"r"))==NULL)
-        	{  
-        		printf("Ã»ÓĞ¸ÃÎÄ¼ş\n\n\n");
-        		scanf("%s%s%s",argv[0],argv[1],argv[2]);
-        		continue;
-        	}
-        else if(!strcmp(argv[1],"-w")) 
-        	{ 
-        		CountW(argv[2]);                
-        	} 
-		else if(!strcmp(argv[1],"-c"))  
-        	{ 
-				CountC(argv[2]);     
-        	} 
-		printf("\n\n");
-        scanf("%s%s%s",argv[0],argv[1],argv[2]);
+        puts("error!\n");
+        return 0;
     }
+    if (argv[1][1] == 'w') // ç»Ÿè®¡å•è¯
+    {
+        char s[1024];
+        while (fscanf(fp, "%s", s) != EOF)
+        {
+            cnt ++;
+            for (int i = 1; i < strlen(s) - 1; i ++)
+                if (s[i] == ',' && s[i - 1] != ',' && s[i + 1] != ',') 
+                    cnt ++;
+        }
+        printf("å•è¯æ•°=%d\n", cnt);
+    }
+    else if (argv[1][1] == 'c') // ç»Ÿè®¡å­—ç¬¦
+    {
+        char c;
+        while ((c = fgetc(fp)) != EOF) cnt ++;
+        printf("å­—ç¬¦æ•°=%d", cnt);
+    }
+    fclose(fp);
     return 0;
 }
 
