4fa57d0bead62a2ec1a495f20adaf11a2485b7a5
ltl531124 <105534369+ltl531124@users.noreply.github.com>
2022-05-21 10:42:26 +0800

    Update WordCount.cpp

--- WordCount.cpp
+++ WordCount.cpp
@@ -1,34 +1,98 @@
-#include <stdio.h>
-#include <string.h>
-int main (int argc, char *argv[])
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int CountW(char *file)//返回文件词的数目 
+{  
+	FILE *fp;
+    char buffer[5000];
+    char c;
+    int bufferlen=0;
+    int LastComma = 0; // 上个字符是否是空格或逗号(1表示是  0表示不是)
+    int WordCount=0;
+    int i;
+    if((fp=fopen(file,"r"))==NULL)
+	{
+        printf("文件不能打开\n");
+        exit(0);
+    }
+	while(fgets(buffer,1000,fp)!=NULL)
+	{	
+        bufferlen=strlen(buffer);
+        for(i=0;i<bufferlen;i++)
+		{
+            c=buffer[i];
+            if(c==' ' ||c==',')
+			{
+                if(LastComma==0)
+				{
+                    WordCount++;
+                }
+                LastComma=1;
+            }
+			else if(c!='\n'&&c!='\r')
+			{
+                LastComma=0;
+            }
+ 
+        } 
+        if(LastComma==0)
+            WordCount++;
+        LastComma=1;
+    }
+    fclose(fp); 
+		printf("WordNum：%d\n",WordCount);                     	
+}
+int CountC(char *file) //返回文件的字符数 
+	{   
+		FILE *fp;
+		int CharCount=0;
+    	fp = fopen(file, "r");
+    	char a;
+    	if(NULL==(fp=fopen(file,"r")))
+    		{
+        		printf("文件为空");
+    		}
+    	while (!feof(fp))
+    		{
+        		a=fgetc(fp);
+                	CharCount++;
+    		}
+    		
+    	fclose(fp);
+		printf("CharNum:%d ",CharCount-1);//作业中没有说明 回车符'\r' 算字符吧 ，所以减去回车符带来的字符数 
+	}
+	
+int main(int argc, char* argv[])             
 {
-    // argc 表示参数个数, *argv[]指针数组, 每一个指针指向一个字符串
     FILE *fp;
-    int cnt = 0; // 统计数
-    if ((fp=fopen(argv[2], "r")) == NULL)
+    while(1)
     {
-        puts("error!\n");
-        return 0;
+    	if(strcmp(argv[1],"-h")&&strcmp(argv[1],"-w")&&strcmp(argv[1],"-c"))
+    		{
+            	printf("没有该功能\n!提示!\n-w 读出单词数量\n-c 读出字符数量\n"); 
+            	return 0;
+    		}
+		if(!strcmp(argv[1],"-h"))
+    		{
+		    	printf("\n-w 读出单词数量\n-c 读出字符数量\n");
+		    	return 0;
+			}
+        if((fp=fopen(argv[2],"r"))==NULL)
+        	{  
+        		printf("没有该文件\n\n\n");
+        		scanf("%s%s%s",argv[0],argv[1],argv[2]);
+        		continue;
+        	}
+        else if(!strcmp(argv[1],"-w")) 
+        	{ 
+        		CountW(argv[2]);                
+        	} 
+		else if(!strcmp(argv[1],"-c"))  
+        	{ 
+				CountC(argv[2]);     
+        	} 
+		printf("\n\n");
+        scanf("%s%s%s",argv[0],argv[1],argv[2]);
     }
-    if (argv[1][1] == 'w') // 统计单词
-    {
-        char s[1024];
-        while (fscanf(fp, "%s", s) != EOF)
-        {
-            cnt ++;
-            for (int i = 1; i < strlen(s) - 1; i ++)
-                if (s[i] == ',' && s[i - 1] != ',' && s[i + 1] != ',') 
-                    cnt ++;
-        }
-        printf("单词数=%d\n", cnt);
-    }
-    else if (argv[1][1] == 'c') // 统计字符
-    {
-        char c;
-        while ((c = fgetc(fp)) != EOF) cnt ++;
-        printf("字符数=%d", cnt);
-    }
-    fclose(fp);
     return 0;
 }
-
